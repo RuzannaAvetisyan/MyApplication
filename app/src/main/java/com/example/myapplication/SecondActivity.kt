2a5e5f7dc0d6ac7b060e1e61c1e4576c435e5264
Varosyan <david.varosyan@picsart.com>
2021-03-15 20:39:29 +0400

    created SecondActivity.kt

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity :AppCompatActivity(){
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
+}
