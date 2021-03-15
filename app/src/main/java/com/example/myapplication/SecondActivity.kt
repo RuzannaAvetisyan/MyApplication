586a4bbf5de52d9766701e1e1beb5ad7b37a63da
Varosyan <david.varosyan@picsart.com>
2021-03-15 20:49:53 +0400

    content view for SecondActivity.kt

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity :AppCompatActivity(){
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
